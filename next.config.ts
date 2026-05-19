3f4ee4f40ae1e148b825ac7b7c73fda6b401a1ea
alihassanii12 <devbench0@gmail.com>
2026-05-19 23:25:25 +0500

    Add environment variables directly in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_URL: 'https://chatweb-backend-mbmu.onrender.com',
+    NEXT_PUBLIC_WS_URL: 'wss://chatweb-backend-mbmu.onrender.com',
+  },
 };
 
 export default nextConfig;
